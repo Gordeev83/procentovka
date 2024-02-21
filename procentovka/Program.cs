3cb867ccabeaff6f7a10a1dac35e19dc8ef84032
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-02-21 08:39:16 +0300

    Добавьте файлы проекта.

---
+++ procentovka/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace procentovka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение: ");
+            double value = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите процент: ");
+            double percent = Convert.ToDouble(Console.ReadLine());
+
+            double result = value * percent / 100;
+
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
